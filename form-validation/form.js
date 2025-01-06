370ada8a7994cc8d89b1f10e31b240f27c492d07
SRIMALARV <128771537+SRIMALARV@users.noreply.github.com>
2025-01-06 10:43:29 +0530

    added css

--- form-validation/form.js
+++ form-validation/form.js
@@ -3,15 +3,9 @@ link.rel = "stylesheet";
 link.href = "style.css";
 document.head.appendChild(link);
 
-function buildHTMLStructure(elements, parentId = "body") {
+function buildHTMLStructure(elements, parentElement = document.body) {
   elements.forEach((element) => {
     const htmlElement = document.createElement(element.tag);
-
-    if (parentId === "body") {
-      document.body.appendChild(htmlElement);
-    } else {
-      document.getElementById(parentId).appendChild(htmlElement);
-    }
     if (element.attributes) {
       for (let attr in element.attributes) {
         htmlElement.setAttribute(attr, element.attributes[attr]);
@@ -28,12 +22,15 @@ function buildHTMLStructure(elements, parentId = "body") {
     if (element.text) {
       htmlElement.innerHTML = element.text;
     }
+    parentElement.appendChild(htmlElement);
+
     if (element.children) {
-      buildHTMLStructure(element.children, element.attributes?.id);
+      buildHTMLStructure(element.children, htmlElement);
     }
   });
 }
 
+// Form structure
 const userForm = [
   {
     tag: "div",
@@ -45,80 +42,128 @@ const userForm = [
         attributes: { id: "userForm", method: "post" },
         children: [
           {
-            tag: "label",
-            attributes: { for: "firstName" },
-            text: "First Name: ",
-          },
-          {
-            tag: "input",
-            attributes: { type: "text", id: "firstName", name: "firstName", required: true },
-          },
-          { tag: "br" },
-          {
-            tag: "label",
-            attributes: { for: "lastName" },
-            text: "Last Name: ",
-          },
-          {
-            tag: "input",
-            attributes: { type: "text", id: "lastName", name: "lastName", required: true },
-          },
-          { tag: "br" },
-          {
-            tag: "label",
-            attributes: { for: "age" },
-            text: "Age: ",
-          },
-          {
-            tag: "input",
-            attributes: { type: "number", id: "age", name: "age", required: true },
-          },
-          { tag: "br" },
-          {
-            tag: "label",
-            attributes: { for: "gender" },
-            text: "Gender: ",
-          },
-          {
-            tag: "select",
-            attributes: { id: "gender", name: "gender", required: true },
+            tag: "div",
             children: [
-              { tag: "option", attributes: { value: "male" }, text: "Male" },
-              { tag: "option", attributes: { value: "female" }, text: "Female" },
-              { tag: "option", attributes: { value: "other" }, text: "Other" },
+              {
+                tag: "div",
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "firstName" },
+                    text: "First Name: ",
+                  },
+                  {
+                    tag: "input",
+                    attributes: { type: "text", id: "firstName", name: "firstName", placeholder: "Enter first name", required: true },
+                  },
+                ],
+              },
+              {
+                tag: "div",
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "lastName" },
+                    text: "Last Name: ",
+                  },
+                  {
+                    tag: "input",
+                    attributes: { type: "text", id: "lastName", name: "lastName", placeholder: "Enter last name", required: true },
+                  },
+                ],
+              },
             ],
           },
-          { tag: "br" },
           {
-            tag: "label",
-            attributes: { for: "email" },
-            text: "Email: ",
+            tag: "div",
+            children: [
+              {
+                tag: "div", 
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "age" },
+                    text: "Age: ",
+                  },
+                  {
+                    tag: "input",
+                    attributes: { type: "number", id: "age", name: "age", placeholder: "Enter age", required: true },
+                  },
+                ],
+              },
+              {
+                tag: "div", 
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "gender" },
+                    text: "Gender: ",
+                  },
+                  {
+                    tag: "select",
+                    attributes: { id: "gender", name: "gender", required: true },
+                    children: [
+                      { tag: "option", attributes: { value: "male" }, text: "Male" },
+                      { tag: "option", attributes: { value: "female" }, text: "Female" },
+                      { tag: "option", attributes: { value: "other" }, text: "Other" },
+                    ],
+                  },
+                ],
+              },
+            ],
           },
           {
-            tag: "input",
-            attributes: { type: "email", id: "email", name: "email", required: true },
+            tag: "div",
+            children: [
+              {
+                tag: "div", 
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "email" },
+                    text: "Email: ",
+                  },
+                  {
+                    tag: "input",
+                    attributes: { type: "email", id: "email", name: "email", placeholder: "Enter email", required: true },
+                  },
+                ],
+              },
+              {
+                tag: "div",
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "password" },
+                    text: "Password: ",
+                  },
+                  {
+                    tag: "input",
+                    attributes: { type: "password", id: "password", name: "password", placeholder: "Enter password", required: true },
+                  },
+                ],
+              },
+            ],
           },
-          { tag: "br" },
           {
-            tag: "label",
-            attributes: { for: "password" },
-            text: "Password: ",
+            tag: "div",
+            children: [
+              {
+                tag: "div", 
+                children: [
+                  {
+                    tag: "label",
+                    attributes: { for: "address" },
+                    text: "Address: ",
+                  },
+                  {
+                    tag: "textarea",
+                    attributes: { id: "address", name: "address", placeholder: "Enter address", rows: "3", cols: "30", required: true },
+                  },
+                ],
+              },
+            ],
           },
-          {
-            tag: "input",
-            attributes: { type: "password", id: "password", name: "password", required: true },
-          },
-          { tag: "br" },
-          {
-            tag: "label",
-            attributes: { for: "address" },
-            text: "Address: ",
-          },
-          {
-            tag: "textarea",
-            attributes: { id: "address", name: "address", rows: "3", cols: "30", required: true },
-          },
-          { tag: "br" },
           {
             tag: "button",
             attributes: { type: "submit" },
